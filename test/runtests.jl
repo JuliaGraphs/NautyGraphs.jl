6dbf2472e7368d747008c1284036ea2c610b9604
Maximilian Hübl <89022871+mxhbl@users.noreply.github.com>
2025-10-31 16:24:20 +0100

    raise julia compat to 1.10; remove tests on 1.6 (#34)
    * raise julia compat to 1.10; remove tests on 1.6
    
    * Update runtests.jl
    
    * Update tests to specify target_modules
    
    * Update CI to use version 1.12
    
    * Update version check for aqua.jl and jet.jl
    
    * change test rng to Xoshiro
    
    * cosmetics
    
    * more costmetics in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,14 @@ using Test
 using Random, LinearAlgebra
 using Base.Threads
 
+rng = Random.Xoshiro(0) 
+
 @testset verbose=true "NautyGraphs" begin
     include("densenautygraph.jl")
     include("nauty.jl")
     include("graphset.jl")
     include("utils.jl")
-    VERSION >= v"1.9-" && include("interface.jl")
-    include("aqua.jl")
-    VERSION >= v"1.10" && include("jet.jl")
+    include("interface.jl")
+    VERSION >= v"1.12" && include("aqua.jl")
+    VERSION >= v"1.12" && include("jet.jl")
 end

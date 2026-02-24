03979776900a2f062b541cfa3ca80759601f9a8b
Maximilian HUEBL <maximilian.huebl@ist.ac.at>
2026-02-24 09:41:55 +0100

    refactor abstractnautygraph

--- src/densenautygraph.jl
+++ src/densenautygraph.jl
@@ -121,54 +121,6 @@ Base.:(==)(g::DenseNautyGraph, h::DenseNautyGraph) = (g.graphset == h.graphset) 
 
 # BASIC GRAPH API
 
-"""
-    labels(g::AbstractNautyGraph)
-
-Return the vertex labels of `g`. 
-
-Do not modify the vector of labels returned. Use [`setlabels!`](@ref) or [`setlabel!`](@ref) instead.
-"""
-@inline labels(g::AbstractNautyGraph) = g._labels
-
-"""
-    label(g::AbstractNautyGraph, i::Integer)
-
-Return the label of vertex `i` of `g`.
-"""
-@inline label(g::AbstractNautyGraph, index::Integer) = g._labels[index]
-
-"""
-    setlabels!(g::AbstractNautyGraph, vertex_labels)
-
-Set the vertex labels of `g` equal to `vertex_labels`.
-"""
-function setlabels!(g::AbstractNautyGraph, vertex_labels)
-    g.iscanon = false
-    g._labels .= vertex_labels
-    return vertex_labels
-end
-
-"""
-    setlabel!(g::AbstractNautyGraph, i::Integer, vertex_label)
-
-Set the label of vertex `i` of `g` equal to `vertex_label`.
-"""
-function setlabel!(g::AbstractNautyGraph, index::Integer, vertex_label)
-    g.iscanon = false
-    g._labels[index] = vertex_label
-    return vertex_label
-end
-
-"""
-    iscanon(g::AbstractNautyGraph)
-
-Return true if `g` has previously been canonized.
-
-`iscanon(g) == false` does not necessarily imply that `g` is not in canonical form, it just means `g` has never
-been explicitly canonized. This function should be considered internal and may be removed in future versions.
-"""
-@inline iscanon(g::AbstractNautyGraph) = g.iscanon
-
 Graphs.nv(g::DenseNautyGraph) = g.graphset.n
 Graphs.ne(g::DenseNautyGraph) = g.ne
 Graphs.vertices(g::DenseNautyGraph) = Base.OneTo(nv(g))
@@ -255,20 +207,6 @@ end
 Base.:(==)(e1::SimpleEdgeIter{<:Graphs.SimpleGraphs.AbstractSimpleGraph}, e2::SimpleEdgeIter{<:DenseNautyGraph}) = e2 == e1
 
 Graphs.is_directed(::Type{<:DenseNautyGraph{D}}) where {D} = D
-Graphs.edgetype(::AbstractNautyGraph) = Graphs.SimpleGraphs.SimpleEdge{Int}
-Base.eltype(::AbstractNautyGraph{T}) where {T} = T
-Base.zero(::G) where {G<:AbstractNautyGraph} = G(0)
-Base.zero(::Type{G}) where {G<:AbstractNautyGraph} = G(0)
-
-function _induced_subgraph(g::AbstractNautyGraph, iter)
-    h, vmap = invoke(Graphs.induced_subgraph, Tuple{AbstractGraph,typeof(iter)}, g, iter)
-    @views h._labels .= g._labels[vmap]
-    h.iscanon = false
-    return h, vmap
-end
-Graphs.induced_subgraph(g::AbstractNautyGraph, iter::AbstractVector{<:Integer}) = _induced_subgraph(g, iter)
-Graphs.induced_subgraph(g::AbstractNautyGraph, iter::AbstractVector{Bool}) = _induced_subgraph(g, iter)
-Graphs.induced_subgraph(g::AbstractNautyGraph, iter::AbstractVector{<:AbstractEdge}) = _induced_subgraph(g, iter)
 
 # GRAPH MODIFY METHODS
 function Graphs.add_edge!(g::DenseNautyGraph, e::Edge)
@@ -282,7 +220,6 @@ function Graphs.add_edge!(g::DenseNautyGraph, e::Edge)
     g.iscanon = false
     return true
 end
-Graphs.add_edge!(g::AbstractNautyGraph, i::Integer, j::Integer) = Graphs.add_edge!(g, edgetype(g)(i, j))
 
 function Graphs.rem_edge!(g::DenseNautyGraph, e::Edge)
     has_vertex(g, e.src) && has_vertex(g, e.dst) || return false
@@ -295,7 +232,6 @@ function Graphs.rem_edge!(g::DenseNautyGraph, e::Edge)
     g.iscanon = false
     return true
 end
-Graphs.rem_edge!(g::AbstractNautyGraph, i::Integer, j::Integer) = Graphs.rem_edge!(g, edgetype(g)(i, j))
 
 function Graphs.add_vertices!(g::DenseNautyGraph, n::Integer; vertex_labels=0)
     vertex_labels isa Number || n != length(vertex_labels) && throw(ArgumentError("Incompatible length: trying to add `n=$n` vertices, but`vertex_labels` has length $(length(vertex_labels))."))
